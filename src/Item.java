1f7db2b050958ddbb8ea0ac54c4848b0c411f298
Ethan Sun <ethankailisun@yahoo.com>
2021-05-24 18:30:23 -0700

    Just added mob damage reducer spell, an armor item, and a possible bunny potion that allows you to move 2 spaces instead of just 1 for a turn

--- src/Item.java
+++ src/Item.java
@@ -1,10 +1,9 @@
 public class Item {
-    public static int healingPotions (int choice, int playerHP) {
-        int startingPlayerHP = 10;
+    public static int healingPotions (int choice, int currentPlayerHP, int maxHP) {
         // healing potion that adds 3 HP back
         if (choice == 1) {
-            if (playerHP + 3 <= startingPlayerHP) {
-                playerHP = playerHP + 3;
+            if (currentPlayerHP + 3 <= maxHP) {
+                currentPlayerHP = currentPlayerHP + 3;
             }
             else {
                 System.out.println("You may not drink this potion right now because your HP is too high.");
@@ -13,8 +12,8 @@ public class Item {
 
         // healing potion that restores half of what's been lost (rounded down)
         if (choice == 2) {
-            if (playerHP + ((startingPlayerHP - playerHP) / 2) <= startingPlayerHP) {
-                playerHP = playerHP + ((startingPlayerHP - playerHP) / 2);
+            if (currentPlayerHP + ((maxHP - currentPlayerHP) / 2) <= maxHP) {
+                currentPlayerHP = currentPlayerHP + ((maxHP - currentPlayerHP) / 2);
             }
             else {
                 System.out.println("You may not drink this potion right now because your HP is too high.");
@@ -23,8 +22,29 @@ public class Item {
 
         // frickin super ninja ultra max healing potion that resets you back to 10
         if (choice == 3) {
-            playerHP = startingPlayerHP;
+            currentPlayerHP = maxHP;
         }
-        return playerHP;
+        return currentPlayerHP;
+    }
+
+    // spell that reduces mob-inflicted damage by half for 2 turns (rounded up)
+    public static void mobDamageReducerSpell(double mobDamage) {
+        int i = 1;
+        while (i <= 2) {
+            mobDamage = (int) Math.ceil(mobDamage / 2.0);
+            i++;
+        }
+    }
+
+    // armor item that reduces mob-inflicted damage by 20%
+    public static double armorItem(double mobDamage) {
+        return mobDamage * 0.8;
+    }
+
+    // I wanted this to be like "if you take this, you can move 2 spaces instead of just one for a turn"
+    // but it may be a little too involved cuz we'd prbly have to rewrite canMove, move, and moveVisual
+    public static int[] bunnyPotion(int position) {
+        int[] filler = {1, 2, 3};
+        return filler;
     }
 }
