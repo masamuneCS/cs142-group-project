0ea3113d9c0830a4d634ecbb9ad0abcccc623127
Ethan Sun <ethankailisun@yahoo.com>
2021-05-24 18:04:15 -0700

    (hopefully this one won't disappear) I created a potions method, let me know what y'all think cuz I haven't actually like run it or anything cuz I have no idea where tf the run button is in intelliJ

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,30 @@
 public class Item {
+    public static int healingPotions (int choice, int playerHP) {
+        int startingPlayerHP = 10;
+        // healing potion that adds 3 HP back
+        if (choice == 1) {
+            if (playerHP + 3 <= startingPlayerHP) {
+                playerHP = playerHP + 3;
+            }
+            else {
+                System.out.println("You may not drink this potion right now because your HP is too high.");
+            }
+        }
+
+        // healing potion that restores half of what's been lost (rounded down)
+        if (choice == 2) {
+            if (playerHP + ((startingPlayerHP - playerHP) / 2) <= startingPlayerHP) {
+                playerHP = playerHP + ((startingPlayerHP - playerHP) / 2);
+            }
+            else {
+                System.out.println("You may not drink this potion right now because your HP is too high.");
+            }
+        }
+
+        // frickin super ninja ultra max healing potion that resets you back to 10
+        if (choice == 3) {
+            playerHP = startingPlayerHP;
+        }
+        return playerHP;
+    }
 }
