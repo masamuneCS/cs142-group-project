487a199cf6f415eef42b8963c6bbcc729d045944
aj porter <blankscriptyt@gmail.com>
2021-05-25 22:02:51 -0700

    Made the program more presentable; and have removed the method that calls Steeve's speech. will be moved to oporate directly in the minigame for ease. call the his diolouge array from NPC

--- src/NPC.java
+++ src/NPC.java
@@ -1,52 +1,31 @@
 import java.util.Scanner;
 
 public class NPC {
-    int phoneAFriend = 0;
 
-    public static String NPCinteraction(Scanner userInput, int helpPoint, int phoneAFriend,
-                                        String numberKeyOptions, boolean Steeve, boolean miniGameTriggered){
-
-        String output = new String("please use the number keys!(if that's what we're using)");
-        //needs to call "phoningFriend" with current "hintPoint" and "phoneAFriend" values when
-        // "phoningFriend" or "phone a friend" is typed into console
-        String input = userInput.toString();
-
-        String[] validInput; // this will be a series of valid inputs the player can put into the system
-                            // that is accepted and will prompt an NPC interaction depending on
-                            // what is typed and if the player is talking to anyone.
-
-        validInput = numberKeyOptions.split(",");
-        if(Steeve == true){
-            steveHarvyDiolouge(miniGameTriggered, validInput);
-
-        }
-        for(int i = 0; i < validInput.length; i++) {
-            if (input.equals(validInput[i])) {
-                phoningFriend(helpPoint, phoneAFriend, validInput.length);
-            } //what is your plan to implement this?
-        }
-        return output;
-    }
-    public static void steveHarvyDiolouge(boolean miniGameTriggered, String[] userCouldInput){ // steeveHary needs to
-                                                                            // activate whenever the minigame
-                                                                                // is triggered
-           if(miniGameTriggered == true){
-               //text plays if this isn't your first time on the show
-           }
-           else{
-               //this text plays if it IS your first time on the show
-
-               miniGameTriggered = true;
-           }
+        public static String[] harveyDio(int goodPoints){
+        //string of diolouge for Steve harvey to choose from
+           String[] DIO;
+            if(goodPoints >= 100){
+                //diolouge to encourage the player and congradulate them on being good at the game
+                String[] goodDio = new String[]{" ", " "};
+                DIO = goodDio;
+            }
+            else{
+                //diolouge to make fun of the player because they arent good at simple tasks
+                String[] badDio = new String[]{" ", " "};
+                DIO = badDio;
+            }
+                return DIO;
         }
 
-    public static String hints(int indexForRiddle){
+
+    public static String[] hints(int indexForRiddle){
         //insert array containing the hints for ALL of the riddles
         // cant really finish till we have a set amount of riddles
         String[] riddleHint;
         riddleHint = new String[]{"hello", "please don't use this in final version"};
 
-        return riddleHint[indexForRiddle];
+        return riddleHint;
     }
     public static String phoningFriend(int helpPoint, int phoneAFriend, int index){
         //the following controls everything that happens when you type "phone a friend" in the console
@@ -59,11 +38,11 @@ public class NPC {
             }
             else{
                 //every other time "phoningFriend" is called
-                responce = ("sodi: need me again? fine i have time");
+                responce = ("sodi: need me again? fine i have time\n");
 
             }
             //this will be what calls and types the hint in the console
-            responce = responce + ("alreight, pay attention, you're only getting this once; ") + hints(index);
+            responce = responce + ("alright, pay attention, you're only getting this once; ") + hints(index);
 
             helpPoint = helpPoint - 1;
             return responce;
