6eb73f21f9c445fe9376253513a148b729d12179
Zac <andersenzb@gmail.com>
2021-05-25 17:53:54 -0700

    Added GameOverException and gameOver method for ending the game. Implemented scanner and ability for player to choose character class and generate a player character.

--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,75 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        //TODO - Create game loop that runs player interactions and calls other class methods to create gameplay.
-        //prompt
-        Character player = Character.buildMage("Bob");
-        System.out.println(player.hp);
+        while(true) { //game loop. All game processes should be inside this loop.
+            try {
+                Character player;
+                System.out.println("What kind of hero are you? Type a number and press enter.\n" +
+                        "1: Warrior. Can take a lot of hits.\n" +
+                        "2: Rogue. Everything balanced, as it should be.\n" +
+                        "3: Mage. Hits very hard, is made of glass.");
+                while(true) {
+                    switch (userInput.nextLine()) {
+                        case "1" : {
+                            System.out.println("What should I call you, warrior? Type a name and press enter.");
+                            String name = userInput.nextLine();
+                            player = Character.buildWarrior(name);
+                            break;
+                        }
+                        case "2" : {
+                            System.out.println("What should I call you, rogue?");
+                            String name = userInput.nextLine();
+                            player = Character.buildRogue(name);
+                            break;
+                        }
+                        case "3" : {
+                            System.out.println("What should I call you, mage?");
+                            String name = userInput.nextLine();
+                            player = Character.buildMage(name);
+                            break;
+                        }
+                        default : {
+                            System.out.println("Enter 1,2, or 3 to choose.");
+                            continue;
+                        }
+                    }
+                    break; //breaks loop
+                }
+                //TODO - Ya know, the rest of the game.
+                gameOver(player, userInput);
+            }
+            catch (GameOverException exception) { //either closes program or restarts game based on player choice in GameOverException.
+                if (exception.playAgain){
+                    continue;
+                }
+                break;
+            }
+        }
     }
 
+    /**
+     * The end of the game
+     * @param player the player character
+     * @param userInput scanner input
+     * @throws GameOverException end the game
+     */
+    public static void gameOver (Character player, Scanner userInput) throws GameOverException {
+        System.out.println(player.playerName + " died. How sad.\nG A M E  O V E R");
+        System.out.println("Do you want to play again Y/n?");
+        boolean playAgain = !userInput.next().equalsIgnoreCase("n");
+        throw new GameOverException(playAgain);
+
+    }
+
+}
+
+/**
+ * GameOverException either restarts or ends the game.
+ */
+class GameOverException extends Exception{
+    boolean playAgain;
+
+    GameOverException(boolean playAgain){
+        this.playAgain = playAgain;
+    }
 }
