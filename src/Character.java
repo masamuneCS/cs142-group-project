3efec9ab28a3f85e57ff1b4e7a10b12e98c799ca
Zac <andersenzb@gmail.com>
2021-05-26 22:25:44 -0700

    added entity class, made Character and Mob extend Entity. Moved common params and methods to Entity.

--- src/Character.java
+++ src/Character.java
@@ -2,25 +2,21 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Scanner;
 
-public class Character {
+public class Character extends Entity {
     /**
      * Character class defines the player character stats and methods for adjusting stats and inventory
      */
     protected String playerName;
     protected String classType;
-    protected int hp;
-    protected int mana;
     protected int resist;
-    private final int maxHP;
-    private final int maxMana;
+
     private final int passiveResist;
     private static final int maxResist = 4;
     protected Item[] inventory = new Item[4]; //Character can carry max four items
 
-    private Character(String playerName, int maxHP, int maxMana, int passiveResist, String classType) {
+    private Character(String playerName, int maxHP, int maxMana, int passiveResist, String classType, int atkStr, int atkSize) {
+        super(maxHP, maxMana, atkStr, atkSize);
         this.playerName = playerName;
-        hp = this.maxHP = maxHP;
-        mana = this.maxMana = maxMana;
         resist = this.passiveResist = passiveResist;
         this.classType = classType;
     }
@@ -30,7 +26,9 @@ public class Character {
             int maxHP = 20;
             int maxMana = 15;
             int passiveResist = 0;
-            return new Character(playerName, maxHP, maxMana, passiveResist, classType);
+            int atkStr = 6;
+            int atkSize = 2;
+            return new Character(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize);
         }
 
         public static Character buildWarrior(String playerName){
@@ -38,7 +36,9 @@ public class Character {
             int maxHP = 25;
             int maxMana = 10;
             int passiveResist = 1;
-            return new Character(playerName, maxHP, maxMana, passiveResist, classType);
+            int atkStr = 6;
+            int atkSize = 2;
+            return new Character(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize);
         }
 
         public static Character buildMage(String playerName){
@@ -46,57 +46,14 @@ public class Character {
             int maxHP = 15;
             int maxMana = 20;
             int passiveResist = 0;
-            return new Character(playerName, maxHP, maxMana, passiveResist, classType);
+            int atkStr = 6;
+            int atkSize = 2;
+            return new Character(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize);
         }
 
     // Stat effecting methods === === === === === === === === === === === === === === === === === === === ===
 
-    /**
-     * method to reduce players hp and kill the player.
-     * @param dmg how much to lower player hp
-     * @param player player character
-     * @param userInput
-     * @throws GameOverException ends the game
-     */
-    public void dmg(int dmg, Character player, Scanner userInput) throws GameOverException{
-        if (player.hp - (dmg - player.resist) > 0){
-            player.hp -= (dmg - player.resist);
-        }
-        else{
-            Game.gameOver(player, userInput);
-        }
-    }
 
-    /**
-     * method to increase player HP
-     * @param heals how much to increase HP
-     * @param player player character
-     */
-    public void heal(int heals, @NotNull Character player){
-        if (player.hp + heals <= player.maxHP ){
-            player.hp += heals;
-        }
-        else {
-            player.hp = player.maxHP;
-        }
-    }
-
-    /**
-     * method to increase or decrease players mana pool
-     * @param manas positive increases mana, negative decreases mana
-     * @param player player character
-     */
-    public void changeMana(int manas, @NotNull Character player){
-        if (player.mana + manas <= player.maxMana && player.mana + manas > 0){
-            player.mana += manas;
-        }
-        else if(player.mana + manas > player.maxMana){
-            player.mana = player.maxMana;
-        }
-        else{
-            player.mana = 0;
-        }
-    }
 
     public void changeResist(int resists, @NotNull Character player){
         if (player.resist + resists > player.passiveResist && player.resist + resists < maxResist){
@@ -114,12 +71,6 @@ public class Character {
     }
     public void alterInventory(Scanner userInput, Character player){
         //TODO - make method to add items to inventory and allow player to drop/replace items
-    }
-
-  //Ability Methods
-
-    public void basicAttack(Mob enemy){
-
     }
 
 }
