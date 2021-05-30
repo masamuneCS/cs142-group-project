2a0ef2e39b6a6ef395dfff508e40d7e471706d24
Ethan Sun <ethankailisun@yahoo.com>
2021-05-29 20:49:12 -0700

    Just added some more questions and whatnot

--- src/MiniGame.java
+++ src/MiniGame.java
@@ -3,7 +3,8 @@ public class MiniGame {
 
         String[] name = new String[]{"Do doubles or ints have decimals?",
                 "What year did America gain its independence?",
-                "What do you have to put at the top of your code when utilizing a Scanner?", " ", " "};
+                "What do you have to put at the top of your code when utilizing a Scanner?",
+                "What's the difference between while loops and for loops?", " "};
         int i = Game.diceRoll(name.length, 5);
         String question = name[i];
         System.out.println(question);
@@ -68,7 +69,7 @@ public class MiniGame {
         }
         if (i == 3) {
             System.out.println("Choose your answer; \n" + "1) Your name \n" + "2) An import statement \n" +
-                    "3) Class name\n" + "4) Main method1");
+                    "3) Class name\n" + "4) Main method");
             System.out.print("Enter your answer here; ");
             while (true) {
                 switch (Game.userInput.nextLine()) {
@@ -81,11 +82,11 @@ public class MiniGame {
                         break;
                     }
                     case "3": {
-                        System.out.println("Oh my gawd, well look atchu, aren't you just the smartest person in the room");
+                        System.out.println("I'm done....");
                         break;
                     }
                     case "4": {
-                        System.out.println("Haha, nice try...");
+                        System.out.println("WHY ARE YOU EVEN HERE RIGHT NOW");
                         break;
                     }
                     default: {
@@ -93,7 +94,37 @@ public class MiniGame {
                         continue;
                     }
                 }
-
+            }
+        }
+        if (i == 4) {
+            System.out.println("Choose your answer; \n" + "1) One compares things and one performs loops \n" +
+                    "2) Nothing, they're the same \n" +
+                    "3) Really just looks since you can rewrite a while loop to resemble a for loop and vice versa\n" +
+                    "4) One's a type and one's a method call");
+            System.out.print("Enter your answer here; ");
+            while (true) {
+                switch (Game.userInput.nextLine()) {
+                    case "1": {
+                        System.out.println("really.... REALLY?!?!");
+                        break;
+                    }
+                    case "2": {
+                        System.out.println("Come on now! You can do better than THAT");
+                        break;
+                    }
+                    case "3": {
+                        System.out.println("DING DING DING, we have a winner!");
+                        break;
+                    }
+                    case "4": {
+                        System.out.println("Just leave bruh");
+                        break;
+                    }
+                    default: {
+                        System.out.println("Enter 1, 2, 3 or 4 to choose.");
+                        continue;
+                    }
+                }
             }
         }
         return 1;
