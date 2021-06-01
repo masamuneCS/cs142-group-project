604676c90824df2e3f48142716842033f009f0ec
Ethan Sun <ethankailisun@yahoo.com>
2021-05-31 17:19:08 -0700

    Took the advice from some warnings 2.0

--- src/MiniGame.java
+++ src/MiniGame.java
@@ -11,146 +11,109 @@ public class MiniGame {
         System.out.println(question);
 
         if (i == 1) {
-            System.out.println("Choose your answer: \n" + "1) Doubles \n" + "2) Ints \n" + "3) Both\n" + "4) Neither");
+            System.out.println("""
+                    Choose your answer:\s
+                    1) Doubles\s
+                    2) Ints\s
+                    3) Both
+                    4) Neither""");
             System.out.print("Enter your answer here; ");
             while (true) {
                 switch (Game.userInput.nextLine()) {
-                    case "1": {
+                    case "1" -> {
                         System.out.println("Correct!");
                         return true;
                     }
-                    case "2": {
-                        System.out.println("Wait... you actually thought an integer had decimals? It's a whole number!");
-                        break;
-                    }
-                    case "3": {
-                        System.out.println("Haha, nice try...");
-                        break;
-                    }
-                    case "4": {
-                        System.out.println("Haha, nice try...");
-                        break;
-                    }
-                    default: {
-                        System.out.println("Enter 1, 2, 3 or 4 to choose.");
-                    }
+                    case "2" -> System.out.println("Wait... you actually thought an integer had decimals? It's a whole number!");
+                    case "3" -> System.out.println("Haha, nice try...");
+                    case "4" -> System.out.println("Haha, you're funny...");
+                    default -> System.out.println("Enter 1, 2, 3 or 4 to choose.");
                 }
 
             }
         }
         if (i == 2) {
-            System.out.println("Choose your answer: \n" + "1) 1492 \n" + "2) 1861 \n" + "3) 1776\n" + "4) 2021");
+            System.out.println("""
+                    Choose your answer:\s
+                    1) 1492\s
+                    2) 1861\s
+                    3) 1776
+                    4) 2021""");
             System.out.print("Enter your answer here; ");
             while (true) {
                 switch (Game.userInput.nextLine()) {
-                    case "1": {
-                        System.out.println("Bruh... Columbus... come on now!");
-                        break;
-                    }
-                    case "2": {
-                        System.out.println("Haha, you stupid...");
-                        break;
-                    }
-                    case "3": {
+                    case "1" -> System.out.println("Bruh... Columbus... come on now!");
+                    case "2" -> System.out.println("Haha, you stupid...");
+                    case "3" -> {
                         System.out.println("Oh my gawd, well look atchu, aren't you just the smartest person in the room");
                         return true;
                     }
-                    case "4": {
-                        System.out.println("Haha, nice try...");
-                        break;
-                    }
-                    default: {
-                        System.out.println("Enter 1, 2, 3 or 4 to choose.");
-                    }
+                    case "4" -> System.out.println("Haha, nice try...");
+                    default -> System.out.println("Enter 1, 2, 3 or 4 to choose.");
                 }
 
             }
         }
         if (i == 3) {
-            System.out.println("Choose your answer: \n" + "1) Your name \n" + "2) An import statement \n" +
-                    "3) Class name\n" + "4) Main method");
+            System.out.println("""
+                    Choose your answer:\s
+                    1) Your name\s
+                    2) An import statement\s
+                    3) Class name
+                    4) Main method""");
             System.out.print("Enter your answer here; ");
             while (true) {
                 switch (Game.userInput.nextLine()) {
-                    case "1": {
-                        System.out.println("I can't with you right now...");
-                        break;
-                    }
-                    case "2": {
+                    case "1" -> System.out.println("I can't with you right now...");
+                    case "2" -> {
                         System.out.println("Yes! Finally! A half-decently smart person.");
                         return true;
                     }
-                    case "3": {
-                        System.out.println("I'm done....");
-                        break;
-                    }
-                    case "4": {
-                        System.out.println("WHY ARE YOU EVEN HERE RIGHT NOW");
-                        break;
-                    }
-                    default: {
-                        System.out.println("Enter 1, 2, 3 or 4 to choose.");
-                    }
+                    case "3" -> System.out.println("I'm done....");
+                    case "4" -> System.out.println("WHY ARE YOU EVEN HERE RIGHT NOW");
+                    default -> System.out.println("Enter 1, 2, 3 or 4 to choose.");
                 }
             }
         }
         if (i == 4) {
-            System.out.println("Choose your answer: \n" + "1) One compares things and one performs loops \n" +
-                    "2) Nothing, they're the same \n" +
-                    "3) Really just looks since you can rewrite a while loop to resemble a for loop and vice versa\n" +
-                    "4) One's a type and one's a method call");
+            System.out.println("""
+                    Choose your answer:\s
+                    1) One compares things and one performs loops\s
+                    2) Nothing, they're the same\s
+                    3) Really just looks since you can rewrite a while loop to resemble a for loop and vice versa
+                    4) One's a type and one's a method call""");
             System.out.print("Enter your answer here; ");
             while (true) {
                 switch (Game.userInput.nextLine()) {
-                    case "1": {
-                        System.out.println("really.... REALLY?!?!");
-                        break;
-                    }
-                    case "2": {
-                        System.out.println("Come on now! You can do better than THAT");
-                        break;
-                    }
-                    case "3": {
+                    case "1" -> System.out.println("really.... REALLY?!?!");
+                    case "2" -> System.out.println("Come on now! You can do better than THAT");
+                    case "3" -> {
                         System.out.println("DING DING DING, we have a winner!");
                         return true;
                     }
-                    case "4": {
-                        System.out.println("Just leave bruh");
-                        break;
-                    }
-                    default: {
-                        System.out.println("Enter 1, 2, 3 or 4 to choose.");
-                    }
+                    case "4" -> System.out.println("Just leave bruh");
+                    default -> System.out.println("Enter 1, 2, 3 or 4 to choose.");
                 }
             }
         }
         if (i == 5) {
-            System.out.println("Choose your answer: \n" + "1) Well duh \n" +
-                    "2) Yes, you need several \n" +
-                    "3) Yes, but just 1\n" +
-                    "4) Well not necessarily because there's a default constructor");
+            System.out.println("""
+                    Choose your answer:\s
+                    1) Well duh\s
+                    2) Yes, you need several\s
+                    3) Yes, but just 1
+                    4) Well not necessarily because there's a default constructor""");
             System.out.print("Enter your answer here; ");
             while (true) {
                 switch (Game.userInput.nextLine()) {
-                    case "1": {
-                        System.out.println("a;sdlfkjie");
-                        break;
-                    }
-                    case "2": {
-                        System.out.println("Nice try");
-                        break;
-                    }
-                    case "3": {
-                        System.out.println("I can't believe they even allowed you to play");
-                        break;
-                    }
-                    case "4": {
+                    case "1" -> System.out.println("a;sdlfkjie");
+                    case "2" -> System.out.println("Nice try");
+                    case "3" -> System.out.println("I can't believe they even allowed you to play");
+                    case "4" -> {
                         System.out.println("Yes... yEs... YES!!!");
                         return true;
                     }
-                    default: {
-                        System.out.println("Enter 1, 2, 3 or 4 to choose.");
-                    }
+                    default -> System.out.println("Enter 1, 2, 3 or 4 to choose.");
                 }
             }
         }
@@ -216,7 +179,6 @@ public class MiniGame {
                 // telling user that their input was invalid because their input didn't meet 1 of the 2 requirements mentioned above
                 catch (NumberFormatException e) {
                     System.out.println(invalidInput);
-                    continue;
                 }
             }
         }
@@ -261,7 +223,6 @@ public class MiniGame {
                 // telling user that their input was invalid because their input didn't meet 1 of the 2 requirements mentioned above
                 catch (NumberFormatException e) {
                     System.out.println(invalidInput);
-                    continue;
                 }
             }
         }
@@ -306,7 +267,6 @@ public class MiniGame {
                 // telling user that their input was invalid because their input didn't meet 1 of the 2 requirements mentioned above
                 catch (NumberFormatException e) {
                     System.out.println(invalidInput);
-                    continue;
                 }
             }
         }
@@ -351,7 +311,6 @@ public class MiniGame {
                 // telling user that their input was invalid because their input didn't meet 1 of the 2 requirements mentioned above
                 catch (NumberFormatException e) {
                     System.out.println(invalidInput);
-                    continue;
                 }
             }
         }
@@ -395,7 +354,6 @@ public class MiniGame {
                 // telling user that their input was invalid because their input didn't meet 1 of the 2 requirements mentioned above
                 catch (NumberFormatException e) {
                     System.out.println(invalidInput);
-                    continue;
                 }
             }
         }
