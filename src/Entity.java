59e6598ea172728c49e5cb90193cb9e659bfea30
Zac <andersenzb@gmail.com>
2021-06-02 13:49:16 -0700

    Added handling for using inventory items. Added ability for player to refuse new item.

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Locale;
 
 public class Entity {
     //players and mobs are entities. Call entity methods when you need to do Stuff.
@@ -21,8 +21,8 @@ public class Entity {
         initiative = this.initBuff = initBuff; //default initiative is always zero, gets rolled at top of encounter
     }
 
-    //Player Character factory methods === === === === === === === === === === === === === === === === === === === ===
-    public static PlayerCharacter buildRogue(String playerName){
+    //Player factory methods === === === === === === === === === === === === === === === === === === === ===
+    public static Player buildRogue(String playerName){
         String classType = "rogue";
         int maxHP = 30;
         int maxMana = 20;
@@ -30,10 +30,10 @@ public class Entity {
         int atkStr = 3;
         int atkSize = 2;
         int initBuff = 8;
-        return new PlayerCharacter(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize, initBuff);
+        return new Player(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize, initBuff);
     }
 
-    public static PlayerCharacter buildWarrior(String playerName){
+    public static Player buildWarrior(String playerName){
         String classType = "warrior";
         int maxHP = 35;
         int maxMana = 15;
@@ -41,10 +41,10 @@ public class Entity {
         int atkStr = 5;
         int atkSize = 2;
         int initBuff = 2;
-        return new PlayerCharacter(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize, initBuff);
+        return new Player(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize, initBuff);
     }
 
-    public static PlayerCharacter buildMage(String playerName){
+    public static Player buildMage(String playerName){
         String classType = "mage";
         int maxHP = 20;
         int maxMana = 20;
@@ -52,7 +52,7 @@ public class Entity {
         int atkStr = 3;
         int atkSize = 2;
         int initBuff = 4;
-        return new PlayerCharacter(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize, initBuff);
+        return new Player(playerName, maxHP, maxMana, passiveResist, classType, atkStr, atkSize, initBuff);
     }
 
 //Mob Factory Methods === === === === === === === === === === === === === === === === === === === ===
@@ -103,12 +103,12 @@ public class Entity {
      * @throws GameOverException ends the game
      */
     public void dmg(int dmg) throws GameOverException{
-        if (this instanceof PlayerCharacter){
-            if (this.hp - (dmg - ((PlayerCharacter) this).resist) > 0){
-                this.hp -= (dmg - ((PlayerCharacter) this).resist);
+        if (this instanceof Player){
+            if (this.hp - (dmg - ((Player) this).resist) > 0){
+                this.hp -= (dmg - ((Player) this).resist);
             }
             else{
-                Game.gameOver((PlayerCharacter) this);
+                Game.gameOver((Player) this);
             }
         }
         else if (this.hp - dmg > 0){
@@ -152,21 +152,26 @@ public class Entity {
 
     //Ability methods === === === === === === === === === === === === === === === === === === === === === === === === ===
 
+    /**
+     * basicAttack uses the entity it was run on as caster, uses casters atkStr and atkSize stat to attack given target
+     * @param target target to damage
+     * @throws GameOverException If player dies
+     */
     public void basicAttack(Entity target) throws GameOverException{
         //damages an enemy target based on entity atk stats
         int dmg = Game.diceRoll(this.atkSize, this.atkStr);
         String targetClass = target.classType;
         target.dmg(dmg);
-        if (this instanceof PlayerCharacter){
+        if (this instanceof Player){
             if (target.hp > 0){
-                System.out.println(((PlayerCharacter) this).playerName + " attacked " + targetClass + " for " + dmg + " damage!");
+                System.out.println(((Player) this).playerName + " attacked " + targetClass + " for " + dmg + " damage!");
             }
             else{
-                System.out.println(((PlayerCharacter) this).playerName + " attacked " + targetClass + " for " + dmg + " damage and killed it!");
+                System.out.println(((Player) this).playerName + " attacked " + targetClass + " for " + dmg + " damage and killed it!");
             }
         }
         else{
-            System.out.println(this.classType + " attacked " + ((PlayerCharacter)target).playerName + " for " + dmg + " damage!");
+            System.out.println(this.classType + " attacked " + ((Player)target).playerName + " for " + dmg + " damage!");
         }
     }
 //Shaman special ability
@@ -190,17 +195,17 @@ public class Entity {
     public void fireball(Entity target) throws GameOverException, IllegalArgumentException {
         //mage only, deals 4d5 damage
         int manaCost = 5;
-        if (this instanceof PlayerCharacter){
+        if (this instanceof Player){
             if (this.classType.equals("mage")){
                 int dmg = Game.diceRoll(4, 5);
                 String targetClass = target.classType;
                 target.dmg(dmg);
                 this.changeMana(-manaCost);
                 if (target.hp > 0){
-                    System.out.println(((PlayerCharacter) this).playerName + " cast FIREBALL on " + targetClass + " for " + dmg + " damage!");
+                    System.out.println(((Player) this).playerName + " cast FIREBALL on " + targetClass + " for " + dmg + " damage!");
                 }
                 else{
-                    System.out.println(((PlayerCharacter) this).playerName + " cast FIREBALL on " + targetClass + " for " + dmg + " damage and killed it!");
+                    System.out.println(((Player) this).playerName + " cast FIREBALL on " + targetClass + " for " + dmg + " damage and killed it!");
                 }
             }
         }
@@ -212,18 +217,18 @@ public class Entity {
     public void sneakAttack(Entity target) throws GameOverException, IllegalArgumentException {
         //rogue only, does 2 dInitiative damage
         int manaCost = 5;
-        if (this instanceof PlayerCharacter){
+        if (this instanceof Player){
             if (this.classType.equals("rogue")){
                 int dmg = Game.diceRoll(3, this.initiative / 2);
                 String targetClass = target.classType;
                 target.dmg(dmg);
                 this.changeMana(-manaCost);
                 if (target.hp > 0){
-                    System.out.println(((PlayerCharacter) this).playerName + " used SNEAK ATTACK on " + targetClass + " for " + dmg + " damage!");
+                    System.out.println(((Player) this).playerName + " used SNEAK ATTACK on " + targetClass + " for " + dmg + " damage!");
                 }
                 else{
-                    System.out.println(((PlayerCharacter) this).playerName + " used SNEAK ATTACK on " + targetClass + " for " + dmg + " damage and killed it!");
-                };
+                    System.out.println(((Player) this).playerName + " used SNEAK ATTACK on " + targetClass + " for " + dmg + " damage and killed it!");
+                }
             }
         }
         else{
@@ -234,17 +239,17 @@ public class Entity {
     public void smash(Entity target) throws GameOverException, IllegalArgumentException {
         //warrior only, does 4 dStrength damage
         int manaCost = 5;
-        if (this instanceof PlayerCharacter){
+        if (this instanceof Player){
             if (this.classType.equals("warrior")){
                 int dmg = Game.diceRoll(4, this.atkStr);
                 String targetClass = target.classType;
                 target.dmg(dmg);
                 this.changeMana(-manaCost);
                 if (target.hp > 0){
-                    System.out.println(((PlayerCharacter) this).playerName + " used SMASH on " + targetClass + " for " + dmg + " damage!");
+                    System.out.println(((Player) this).playerName + " used SMASH on " + targetClass + " for " + dmg + " damage!");
                 }
                 else{
-                    System.out.println(((PlayerCharacter) this).playerName + " used SMASH on " + targetClass + " for " + dmg + " damage and killed it!");
+                    System.out.println(((Player) this).playerName + " used SMASH on " + targetClass + " for " + dmg + " damage and killed it!");
                 }
             }
         }
@@ -252,22 +257,19 @@ public class Entity {
             throw new IllegalArgumentException();
         }
     }
-
-
 }
 
-
-class PlayerCharacter extends Entity {
+class Player extends Entity {
     /**
      * Character class defines the player character stats and methods for adjusting stats and inventory
      */
     protected String playerName;
-    protected int resist;
+    protected int resist; //reduces all incoming damage by resist
     private final int passiveResist;
     private static final int maxResist = 4;
     protected Item[] inventory = new Item[4]; //Character can carry max four items
 
-    protected PlayerCharacter(String playerName, int maxHP, int maxMana, int passiveResist, String classType, int atkStr, int atkSize, int initBuff) {
+    protected Player(String playerName, int maxHP, int maxMana, int passiveResist, String classType, int atkStr, int atkSize, int initBuff) {
         super(maxHP, maxMana, atkStr, atkSize, classType, initBuff);
         this.playerName = playerName;
         resist = this.passiveResist = passiveResist;
@@ -286,7 +288,12 @@ class PlayerCharacter extends Entity {
             this.resist = maxResist;
         }
     }
-    public void accessInventory(){
+
+    /**
+     * accessInventory allows the player to see what items are in their inventory and use their items
+     * @throws GameOverException Shouldn't ever actually get thrown, bomb calls dmg method which throws GameOverException in case of player death.
+     */
+    public void accessInventory() throws GameOverException {
         System.out.println("Inventory:");
         for (int i = 0; i < this.inventory.length; i++){
             if (this.inventory[i] == null){
@@ -296,10 +303,49 @@ class PlayerCharacter extends Entity {
                 System.out.println("Slot " + (i + 1) + ": " + this.inventory[i].name);
             }
         }
-        while (true){
-            System.out.println("Which item would you like to use? Or choose 5 to go back:");
+
+        System.out.println("Which item would you like to use? Or choose 5 to go back:");
+        int input = Game.inputValidation(5);
+        if (input < 5){
+            input--;
+            switch(this.inventory[input].itemType){
+                case "plusHealth":
+                case "plusMana":
+                    this.inventory[input].use(this);
+                    this.inventory[input] = null;
+                    break;
+                case "plusResist":
+                    if (Game.encounterActive){
+                        this.inventory[input].use(this);
+                        this.inventory[input] = null;
+                    }
+                    else{
+                        System.out.println("You can only use this during an encounter!");
+                    }
+                    break;
+                case "bomb": {
+                    if (Game.encounterActive) {
+                        for (Entity target : Game.encounterMobs) {
+                            if (!target.classType.equals("dead")){
+                                this.inventory[input].use(target);
+                            }
+                        }
+                    } else {
+                        System.out.println("You can only use a bomb in an encounter!");
+                    }
+                    break;
+                }
+                default: {
+                    throw new IllegalArgumentException("Inventory tried to use unknown item type, got " + this.inventory[input].itemType + " for object " + this.inventory[input]);
+                }
+            }
         }
     }
+
+    /**
+     * alterInventory adds new items to the players inventory
+     * @param newItem item generated by loot table
+     */
     public void alterInventory(Item newItem){
         for (int i = 0; i < this.inventory.length; i++){
             if (this.inventory[i] == null){
@@ -308,35 +354,44 @@ class PlayerCharacter extends Entity {
             else{
                 System.out.print("Slot " + (i + 1) + ": " + this.inventory[i].name);
             }
-
         }
-        System.out.println("What slot would you like to add the item to? Or choose 5 to go back.");
-        while(true) {
-            switch (Game.userInput.nextLine()) {
-                case "1": {
-                    this.inventory[0] = newItem;
-                    break;
-                }
-                case "2": {
-                    this.inventory[1] = newItem;
-                    break;
-                }
-                case "3": {
-                    this.inventory[2] = newItem;
-                    break;
-                }
-                case "4": {
-                    this.inventory[3] = newItem;
-                    break;
-                }
-                case "5": {
 
+        while(true) {
+            System.out.println("What slot would you like to add the item to? Or choose 5 to throw away the new item.");
+            int input = Game.inputValidation(5);
+            if (input < 5){
+                if (this.inventory[input - 1] == null){
+                    this.inventory[input - 1] = newItem;
                 }
-                default: {
-                    System.out.println("Enter 1,2, 3, or 4 to choose.");
-                    continue;
+                else{
+                    System.out.println("There is a " + this.inventory[input -1].name + " in that slot, are you sure? [y,N]");
+                    String strInput = Game.userInput.nextLine();
+                    switch(strInput.toUpperCase(Locale.ROOT)){
+                        case "Y":{
+                            System.out.println("You threw away " + this.inventory[input - 1] + " and kept " + newItem);
+                            this.inventory[input - 1] = newItem;
+                            break;
+                        }
+                        case "N":
+                        default: {
+                            continue;
+                        }
+                    }
                 }
-                //TODO - confirm if player wants to throw away item in occupied slot, allow them to just not pick up item
+            }
+            else {
+                System.out.println("Are you sure you want to throw away the " + newItem + "? [y/N]");
+                String strInput = Game.userInput.nextLine();
+                switch(strInput.toUpperCase(Locale.ROOT)){
+                    case "Y":{
+                        System.out.println("You threw away " + newItem);
+                        break;
+                    }
+                    case "N":
+                    default: {
+                        continue;
+                    }
+                }
             }
             break;
         }
@@ -348,5 +403,4 @@ class Mob extends Entity {
     protected Mob(String classType, int maxHP, int maxMana, int atkStr, int atkSize, int initBuff){
         super(maxHP, maxMana, atkStr, atkSize, classType, initBuff);
     }
-
 }
