84160adb48068e9f8a194c4d13a78273cab24237
Zac <andersenzb@gmail.com>
2021-06-01 19:00:03 -0700

    Completed encounter method. Did some rebalancing and added more info text and delays for clarity to the player. Added mob logic for attacking.

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,12 @@
 import javax.naming.NoInitialContextException;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Game {
     public static Scanner userInput = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        MiniGame.blackJack();
+    public static void main(String[] args){
         int gameStage = 1;
         while(true) { //game loop. All game processes should be inside this loop.
             try { //KEEP ALL GAME CODE INSIDE THE TRY BLOCK PLEASE AND THANK YOU
@@ -98,6 +98,7 @@ public class Game {
             if (userInput.hasNextInt()){
                 int input = userInput.nextInt();
                 if (input <= ceiling && input > 0){
+                    userInput.nextLine();
                     return input;
                 }
                 else{
@@ -112,25 +113,26 @@ public class Game {
     }
 
     public static boolean encounter(PlayerCharacter player, int gameStage) throws GameOverException, IllegalArgumentException {
-        Mob[] encounterMobs = new Mob[] {Entity.buildDead(), Entity.buildDead(), Entity.buildDead(), Entity.buildDead()};
+        boolean encounterActive = true;
+        Mob[] encounterMobs = new Mob[] {Entity.buildDead(), Entity.buildDead(), Entity.buildDead(), Entity.buildDead(), Entity.buildDead()};
         int mobCount = diceRoll(1, 3);
 
         for (int i = 0; i < mobCount; i++){
-            switch(diceRoll(1, 3)){ //picking mobType based on int 1-3
-                case 1:
-                    encounterMobs[i] = Entity.buildGrunt();
-                    break;
-                case 2:
-                    encounterMobs[i] = Entity.buildBrute();
-                    break;
-                case 3:
-                    encounterMobs[i] = Entity.buildShaman();
-                    break;
-                default :
-                    throw new IllegalArgumentException("Encounter mob builder received invalid mob type roll");
+            int roll = diceRoll(1, 10); //picking mobType based on int 1-10
+            if (roll >= 1 && roll < 6) {
+                encounterMobs[i] = Entity.buildGrunt();
+            }
+            else if (roll >= 6 && roll < 9) {
+                encounterMobs[i] = Entity.buildBrute();
+            }
+            else if (roll >= 9 && roll < 11) {
+                encounterMobs[i] = Entity.buildShaman();
+            }
+            else {
+                throw new IllegalArgumentException("Encounter mob builder received invalid mob type roll");
             }
         }
-        Entity[] encounterAll = new Entity[5]; //placing player in entity array w/ mobs
+        Entity[] encounterAll = new Entity[6]; //placing player in entity array w/ mobs
         encounterAll[0] = player;
         System.arraycopy(encounterMobs, 0, encounterAll, 1, encounterMobs.length);
         //establish initiative for player and mobs.
@@ -143,18 +145,44 @@ public class Game {
         for (int i = 1; i < encounterAll.length; i++){
             Entity key = encounterAll[i];
             int j = i - 1;
-            while (j >= 0 && encounterAll[j].initiative > key.initiative){
+            while (j >= 0 && encounterAll[j].initiative < key.initiative){
                 encounterAll[j+1] = encounterAll[j];
                 j--;
             }
             encounterAll[j+1] = key;
 
         }
-        int turn = 0;
-        while(true){
-            int movePoint = 1;
+        int turn = -2;
+        boolean allMobsDead = false;
+        while(true){ // encounter combat turns loop
+            turn++;
+            for (Mob mob : encounterMobs){
+                allMobsDead = true;
+                if (!mob.classType.equals("dead")){
+                    allMobsDead = false;
+                    break;
+                }
+            }
+            if (allMobsDead){
+                System.out.println("You are victorious over your slain enemies!");
+                return true; //all mobs are dead, player has won the encounter
+            }
+            mobCount = 0;
+            for (Mob mob : encounterMobs){
+                if (!mob.classType.equals("dead")){
+                    mobCount ++;
+                }
+            }
+            if (turn >= encounterAll.length - 1 || turn == -1){
+                turn = 0;
+            }
             if (encounterAll[turn] instanceof PlayerCharacter){
-                while(movePoint > 0){
+                while(true){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        System.err.println("Slept thread was interrupted");
+                    }
                     if (mobCount > 1){
                         System.out.println("There are " + mobCount + " enemies before you!");
                     }
@@ -174,28 +202,49 @@ public class Game {
                             }
                             System.out.println("or choose " + encounterMobs.length + " to go back.");
                             System.out.print("Choose: ");
-                            int input = inputValidation(encounterMobs.length);
-                            if (input < mobCount) {
-                                player.basicAttack(encounterMobs[input - 1]);
-                                movePoint -= 1;
-                            } else {
+                            int input = inputValidation(encounterMobs.length) - 1;
+                            if (encounterMobs[input].classType.equals("dead")){
+                                System.out.println("There's nothing there for you to attack!");
                                 continue;
+                            }
+                            else {
+                                player.basicAttack(encounterMobs[input]);
                             }
                             break;
                         }
                         case "2": {
                             if (player.classType.equals("warrior")) {
                                 System.out.println(player.playerName + " your special warrior ability is SMASH. SMASH deals 4d" + player.atkStr + " damage and costs 5 mana");
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    System.err.println("Slept thread was interrupted");
+                                }
                             }
                             else if (player.classType.equals("rogue")){
                                 System.out.println(player.playerName + " your special rogue ability is SNEAK ATTACK. SNEAK ATTACK does damage based on your initiative and costs 5 mana. \nThis encounter it does 2d" + (player.initiative / 2) + " damage.");
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    System.err.println("Slept thread was interrupted");
+                                }
                             }
                             else if (player.classType.equals("mage")){
                                 System.out.println(player.playerName + " your special mage ability is FIREBALL. FIREBALL does 2d8 damage and costs 5 mana.");
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    System.err.println("Slept thread was interrupted");
+                                }
                             }
                             if (player.mana < 5){
                                 System.out.println("You don't have enough mana, maybe try using a potion?");
                                 System.out.println();
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    System.err.println("Slept thread was interrupted");
+                                }
                                 continue;
                             }
                             System.out.println("Which enemy would you like to attack?\n or press " + encounterMobs.length + " to go back.");
@@ -206,40 +255,83 @@ public class Game {
                                 i++;
                             }
                             System.out.println(encounterMobs.length + ": Go back");
-                            int input = inputValidation(encounterMobs.length);
-                            if (input <= mobCount){
-                                if (player.classType.equals("warrior")){
-                                    player.smash(encounterMobs[input]);
-                                    movePoint -= 1;
+                            int input = inputValidation(5) - 1;
+                            if (input < encounterMobs.length){
+                                if (encounterMobs[input].classType.equals("dead")){
+                                    System.out.println("There's nothing there for you to attack!");
+                                    continue;
                                 }
-                                else if (player.classType.equals("mage")){
-                                    player.fireball(encounterMobs[input]);
-                                    movePoint -= 1;
+                                switch (player.classType) {
+                                    case "warrior":
+                                        player.smash(encounterMobs[input]);
+                                        break;
+                                    case "mage":
+                                        player.fireball(encounterMobs[input]);
+                                        break;
+                                    case "rogue":
+                                        player.sneakAttack(encounterMobs[input]);
+                                        break;
+                                    default:
+                                        throw new IllegalArgumentException("Unexpected player.classType in encounter special attack line 214");
                                 }
-                                else if (player.classType.equals("rogue")){
-                                    player.sneakAttack(encounterMobs[input]);
-                                }
-                                else {
-                                    throw new IllegalArgumentException("player.classType did not match any special ability class");
-                                }
+                            }
+                            else{
+                                continue;
                             }
                             break;
                         }
                         case "3": {
                             System.out.println("Using an inventory item does NOT end your turn.");
                             player.accessInventory();
+                            continue;
                         }
+                    }//End switch case
+                    break;
+                }//End player interaction loop
+            }//End player turn options
+            //Mob combat logic
+            else if (encounterAll[turn] instanceof Mob){
+                /*
+                Mobs will attack player until death.
+                Shaman will check to see if any allies are below half health and attempt to heal them or else attack player.
+                 */
+                switch(encounterAll[turn].classType){
+                    case "dead": {
+                        continue;
                     }
+                    case "grunt":
+                    case "brute": {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            System.err.println("Slept thread was interrupted");
+                        }
+                        encounterAll[turn].basicAttack(player);
+                        continue;
+                    }
+                    case "shaman": {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            System.err.println("Slept thread was interrupted");
+                        }
+                        for (Mob mob : encounterMobs) {
+                            if (mob != encounterAll[turn]) { //this logic is very stupid and it might behoove me to sort for which ally has the LOWEST HP if time allows.
+                                if (mob.hp < mob.maxHP / 2 && encounterAll[turn].mana >= 5 && !mob.classType.equals("dead")) {
+                                    encounterAll[turn].healAlly(mob);
+                                    continue;
+                                }
+                            }
+                        }
+                        encounterAll[turn].basicAttack(player);
+                        continue;
+                    }
+                    default:
+                        throw new IllegalArgumentException("Unexpected mob.classType in encounter combat logic line 244");
+
                 }
             }
-            else{
-                //TODO - Mob action logic
-            }
-            if (turn >= encounterAll.length){
-                turn = 0;
-            }
-            turn += 1;
-        }
+        }//End encounter combat loop
     }
 
 }
