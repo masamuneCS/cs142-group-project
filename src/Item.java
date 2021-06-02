a0ceb8263939c76e90ea62d4b7a5c0e1644f8280
Zac <andersenzb@gmail.com>
2021-06-02 12:34:13 -0700

    Reworked items to use size and strength stats instead of rolling before item creation. Added use method for using items.

--- src/Item.java
+++ src/Item.java
@@ -1,75 +1,107 @@
 public class Item {
-    public int heals;
-    public int damage;
-    public int resist;
-    public int manas;
+    protected String itemType;
+    public int size;
+    public int strength;
     public String name;
 
-    public Item(int heals, int damage, int resist, int manas, String name){
-        this.heals = heals;
-        this.damage = damage;
-        this.resist = resist;
-        this.manas = manas;
+    public Item(String itemType, int size, int strength, String name){
+        this.itemType = itemType;
+        this.size = size;
+        this.strength = strength;
         this.name = name;
     }
 
+    //FACTORY METHODS
     // healing potion that adds 3 HP back
-    public static Item smallHealingPotion () {
-        int heals = Game.diceRoll(2, 4);
-        int damage = 0;
-        int resist = 0;
-        int manas = 0;
-        String name = "Randomized healing potion";
-        return new Item(heals, damage, resist, manas, name);
+    public static Item smallHealPotion () {
+        String itemType = "plusHealth";
+        int size = 2;
+        int strength = 4;
+        String name = "Poultice heals " + size + "d" + strength + " HP.";
+        return new Item(itemType, size, strength, name);
     }
 
     // medium healing potion
-    public static Item mediumHealingPotion () {
-        int heals = Game.diceRoll(2,6);
-        int damage = 0;
-        int resist = 0;
-        int manas = 0;
-        String name = "Plus half of what was lost Healing potion";
-        return new Item(heals, damage, resist, manas, name);
+    public static Item mediumHealPotion () {
+        String itemType = "plusHealth";
+        int size = 3;
+        int strength = 5;
+        String name = "Potion of Health heals " + size + "d" + strength + " HP.";
+        return new Item(itemType, size, strength, name);
     }
 
     // big-ass healing potion
-    public static Item healingMaxPotion () {
-        int heals = Game.diceRoll(3,6);
-        int damage = 0;
-        int resist = 0;
-        int manas = 0;
-        String name = "Frickin' super ninja ultra max healing potion that resets you back to full HP";
-        return new Item(heals, damage, resist, manas, name);
+    public static Item largeHealPotion () {
+        String itemType = "plusHealth";
+        int size = 4;
+        int strength = 6;
+        String name = "Draught of health heals " + size + "d" + strength + " HP.";
+        return new Item(itemType, size, strength, name);
     }
 
     // spell that reduces mob-inflicted damage by 1
-    public static Item mobDamageReducerSpell() {
-        int heals = 0;
-        int damage = 0;
-        int resist = Game.diceRoll(1, 5);
-        int manas = 0;
-        String name = "Mob damage reducer spell";
-        return new Item(heals, damage, resist, manas, name);
+    public static Item resistPot() {
+        String itemType = "plusResist";
+        int size = 1;
+        int strength = 3;
+        String name = "Draught of Armor gives " + size + "d" + strength + " resistance.";
+        return new Item(itemType, size, strength, name);
     }
 
-    // armor item that reduces mob-inflicted damage by.....
-    public static Item armorItem() {
-        int heals = 0;
-        int damage = 0;
-        int resist = Game.diceRoll(1, 5);
-        int manas = 0;
-        String name = "Armor";
-        return new Item(heals, damage, resist, manas, name);
+    // mana potion - coffee
+    public static Item smallManaPot() {
+        String itemType = "plusMana";
+        int size = 3;
+        int strength = 4;
+        String name = "Potion of Rest restores " + size + "d" + strength + " mana.";
+        return new Item(itemType, size, strength, name);
     }
 
-    // mana potion
-    public static Item manaPotion() {
-        int heals = 0;
-        int damage = 0;
-        int resist = 0;
-        int manas = Game.diceRoll(1, 5);
-        String name = "Mana potion";
-        return new Item(heals, damage, resist, manas, name);
+    // mana potion - cocaine
+    public static Item largeManaPot() {
+        String itemType = "plusMana";
+        int size = 5;
+        int strength = 4;
+        String name = "Draught of Rest restores " + size + "d" + strength + " mana.";
+        return new Item(itemType, size, strength, name);
     }
+
+    public static Item bomb(){
+        String itemType = "bomb";
+        int size = 3;
+        int strength = 4;
+        String name = "Firebomb deals " + size + "d" + strength + " damage to all enemies.";
+        return new Item(itemType, size, strength, name);
+    }
+
+    //USE METHODS
+    public void use (Entity target) throws IllegalArgumentException, GameOverException {
+        switch(this.itemType){
+            case "plusHealth":{
+                target.heal(Game.diceRoll(this.size, this.strength));
+                break;
+            }
+            case "plusMana":{
+                target.changeMana(Game.diceRoll(this.size, this.strength));
+                break;
+            }
+            case "plusResist": {
+                if (target instanceof Player){
+                    ((Player) target).changeResist(Game.diceRoll(this.size, this.strength));
+                    break;
+                }
+                else{
+                    throw new IllegalArgumentException("Item.use plusResist expected Entity subclass Player got " + target.getClass());
+                }
+            }
+            case "bomb":{
+                target.dmg(Game.diceRoll(this.size, this.strength));
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Item.use received unknown itemType from " + this);
+        }
+
+    }
+
 }
